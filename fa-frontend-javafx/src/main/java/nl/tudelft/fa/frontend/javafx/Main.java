ce37010581f1ff38064cb2ee00f30da0d0573351
Fabian Mastenbroek <mail.fabianm@gmail.com>
2016-11-28 20:34:26 +0100

    Fix indentation in respect to checkstyle configuration

--- fa-frontend-javafx/src/main/java/nl/tudelft/fa/frontend/javafx/Main.java
+++ fa-frontend-javafx/src/main/java/nl/tudelft/fa/frontend/javafx/Main.java
@@ -1,3 +1,28 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Fabian Mastenbroek, Christian Slothouber,
+ * Laetitia Molkenboer, Nikki Bouman, Nils de Beukelaar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package nl.tudelft.fa.frontend.javafx;
 
 import javafx.application.Application;
@@ -12,15 +37,17 @@ import javafx.stage.Stage;
  * @author Fabian Mastenbroek
  */
 public class Main extends Application {
-	/** {@inheritDoc} */
-	@Override
-	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
 
-		Scene scene = new Scene(root, 300, 275);
+        Scene scene = new Scene(root, 300, 275);
 
-		stage.setTitle("Formula Andy!");
-		stage.setScene(scene);
-		stage.show();
-	}
+        stage.setTitle("Formula Andy!");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
