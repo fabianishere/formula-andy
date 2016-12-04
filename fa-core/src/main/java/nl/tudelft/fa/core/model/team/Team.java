090e82a5bf726cd7b9db994562dd03bb7b6780d6
Fabian Mastenbroek <mail.fabianm@gmail.com>
2016-12-04 13:56:18 +0100

    Finish Team class

--- fa-core/src/main/java/nl/tudelft/fa/core/model/team/Team.java
+++ fa-core/src/main/java/nl/tudelft/fa/core/model/team/Team.java
@@ -27,40 +27,135 @@ package nl.tudelft.fa.core.model.team;
 
 import nl.tudelft.fa.core.controller.team.Manager;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
 
 /**
- * This class represents a Formula 1 team participating in
+ * This class represents a Formula 1 team that participates in
  * {@link nl.tudelft.fa.core.model.game.GrandPrix}s.
  *
  * @author Christian Slothouber
  */
 public class Team {
     /**
-     * The team profile of this team.
+     * The unique id of this team.
      */
-    private TeamProfile profile;
+    private final UUID id;
+
+    /**
+     * The name of this team.
+     */
+    private final String name;
+
+    /**
+     * The current budget of the team.
+     */
+    private int budget;
 
     /**
      * The staff members of this team.
      */
-    private List<Member> staff;
+    private final List<Member> staff;
 
     /**
      * The manager that manages this team.
      */
-    private Manager manager;
+    private final Manager manager;
 
     /**
      * Construct a {@link Team} instance.
      *
-     * @param profile The profile of this team.
-     * @param staff The staff members of this team.
+     * @param name The name of the team.
+     * @param budget The budget of the team.
      * @param manager The manager that manages this team.
+     * @param staff The staff members of this team.
      */
-    public Team(TeamProfile profile, List<Member> staff, Manager manager) {
-        this.profile = profile;
-        this.staff = staff;
+    public Team(UUID id, String name, int budget, Manager manager, List<Member> staff) {
+        this.id = id;
+        this.name = name;
+        this.budget = budget;
         this.manager = manager;
+        this.staff = staff;
+    }
+
+    /**
+     * Return the unique id of this team.
+     *
+     * @return The unique id of this team/
+     */
+    public UUID getId() {
+        return id;
+    }
+
+    /**
+     * Return the name of the team.
+     *
+     * @return The name of the team.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Return the budget of the team.
+     *
+     * @return The budget of the team.
+     */
+    public int getBudget() {
+        return budget;
+    }
+
+    /**
+     * Return the manager of this team.
+     *
+     * @return The manager of this team.
+     */
+    public Manager getManager() {
+        return manager;
+    }
+
+    /**
+     * Return the staff members of the team.
+     *
+     * @return A list of the members of this team.
+     */
+    public List<Member> getStaff() {
+        return Collections.unmodifiableList(staff);
+    }
+
+    /**
+     * Test whether this {@link Team} is equal to the given object.
+     *
+     * @param other The object to be tested for equality
+     * @return <code>true</code> if both objects are equal, <code>false</code> otherwise.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof Team) {
+            Team that = (Team) other;
+            return this.id.equals(that.id);
+        }
+        return false;
+    }
+
+    /**
+     * Return the hash code of this object.
+     *
+     * @return The hash code of this object as integer.
+     */
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    /**
+     * Return a string representation of this team.
+     *
+     * @return A string representation of this team.
+     */
+    @Override
+    public String toString() {
+        return String.format("Team(id=%s, name=%s)", id, name);
     }
 }
