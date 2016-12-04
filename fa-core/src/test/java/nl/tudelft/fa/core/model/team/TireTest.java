520f882f2060f3c4263e0d67671d13fe297435d5
Fabian Mastenbroek <mail.fabianm@gmail.com>
2016-12-04 13:16:41 +0100

    Add test for Tire class

---
+++ fa-core/src/test/java/nl/tudelft/fa/core/model/team/TireTest.java
@@ -0,0 +1,91 @@
+package nl.tudelft.fa.core.model.team;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Objects;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+
+/**
+ * @author Fabian Mastenbroek <mail.fabianm@gmail.com>
+ */
+public class TireTest {
+
+    String brand;
+    String name;
+    double durability;
+    double grip;
+    Tire tireA;
+
+    @Before
+    public void setUp() {
+        brand = "Pirelli";
+        name = "Intermediate";
+        durability = 1.0;
+        grip = 2.0;
+        tireA = new Tire(brand, name, durability, grip);
+    }
+
+    @Test
+    public void getBrand() {
+        assertEquals(brand, tireA.getBrand());
+    }
+
+    @Test
+    public  void getName() {
+        assertEquals(name, tireA.getName());
+    }
+
+    @Test
+    public void getDurability() {
+        assertEquals(durability, tireA.getDurability(), 0.01);
+    }
+
+    @Test
+    public void getGrip() {
+        assertEquals(grip, tireA.getGrip(), 0.01);
+    }
+
+    @Test
+    public void equalsNull() {
+        assertThat(tireA, not(equalTo(null)));
+    }
+
+    @Test
+    public void equalsReference() {
+        assertEquals(tireA, tireA);
+    }
+
+    @Test
+    public void equalsData() {
+        assertEquals(new Tire(brand, name, durability, grip), tireA);
+    }
+
+    @Test
+    public void equalsDifferentBrand() {
+        assertThat(tireA, not(equalTo(new Tire("Silverstone", name, durability, grip))));
+    }
+
+    @Test
+    public void equalsDifferentType() {
+        assertThat(tireA, not(equalTo(new Tire(brand, "Soft", durability, grip))));
+    }
+
+    @Test
+    public void equalsOtherPropertiesHaveNoEffect() {
+        assertEquals(new Tire(brand, name, durability, grip), tireA);
+    }
+
+    @Test
+    public void testHashCode() {
+        assertEquals(Objects.hash(brand, name), tireA.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(String.format("%s %s", brand, name), tireA.toString());
+    }
+}
