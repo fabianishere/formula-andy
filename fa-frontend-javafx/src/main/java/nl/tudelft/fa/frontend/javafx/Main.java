3a9dcda78dc09c7f0899a85a6890e3188e407663
Laetitia Molkenboer <l.e.molkenboer@student.tudelft.nl>
2016-12-05 16:16:59 +0100

    Add login screen

--- fa-frontend-javafx/src/main/java/nl/tudelft/fa/frontend/javafx/Main.java
+++ fa-frontend-javafx/src/main/java/nl/tudelft/fa/frontend/javafx/Main.java
@@ -44,7 +44,7 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
 
         Scene scene = new Scene(root, 300, 275);
 
