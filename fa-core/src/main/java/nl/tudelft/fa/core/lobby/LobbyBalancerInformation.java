0dc8e881edb7dfa6c8824255fa8a34ba6ed2af41
Fabian Mastenbroek <mail.fabianm@gmail.com>
2017-01-03 16:23:00 +0100

    [FEATURE-#85] Create a simple lobby balancer
    This change adds a simple lobby balancer that spins up
    new lobbies on demand and routes players to a lobby.

---
+++ fa-core/src/main/java/nl/tudelft/fa/core/lobby/LobbyBalancerInformation.java
@@ -0,0 +1,99 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Fabian Mastenbroek, Christian Slothouber,
+ * Laetitia Molkenboer, Nikki Bouman, Nils de Beukelaar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package nl.tudelft.fa.core.lobby;
+
+import akka.actor.ActorRef;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * This class provides information about a {@link nl.tudelft.fa.core.lobby.actor.LobbyBalancer}
+ * actor.
+ *
+ * @author Fabian Mastenbroek
+ */
+public class LobbyBalancerInformation {
+    /**
+     * A {@link Map} containing the lobbies the balancer manages.
+     */
+    private Map<ActorRef, LobbyInformation> lobbies;
+
+    /**
+     * Construct a {@link LobbyBalancerInformation} instance.
+     *
+     * @param lobbies The lobbies the balancer manages.
+     */
+    public LobbyBalancerInformation(Map<ActorRef, LobbyInformation> lobbies) {
+        this.lobbies = lobbies;
+    }
+
+    /**
+     * Return the {@link Lobby} actors the balancer manages.
+     *
+     * @return The lobbies the balancer manages.
+     */
+    public Map<ActorRef, LobbyInformation> getLobbies() {
+        return lobbies;
+    }
+
+    /**
+     * Test whether this {@link LobbyBalancerInformation} is equal to the given object.
+     *
+     * @param other The object to be tested for equality
+     * @return <code>true</code> if both objects are equal, <code>false</code> otherwise.
+     */
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        LobbyBalancerInformation that = (LobbyBalancerInformation) other;
+        return Objects.equals(lobbies, that.lobbies);
+    }
+
+    /**
+     * Return the hash code of this object.
+     *
+     * @return The hash code of this object as integer.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(lobbies);
+    }
+
+    /**
+     * Return a string representation of this configuration.
+     *
+     * @return A string representation of this configuration.
+     */
+    @Override
+    public String toString() {
+        return String.format("LobbyBalancerInformation(lobbies=%s)", lobbies);
+    }
+}
---
+++ fa-core/src/main/java/nl/tudelft/fa/core/lobby/actor/LobbyBalancer.java
@@ -0,0 +1,233 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Fabian Mastenbroek, Christian Slothouber,
+ * Laetitia Molkenboer, Nikki Bouman, Nils de Beukelaar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package nl.tudelft.fa.core.lobby.actor;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import akka.japi.pf.ReceiveBuilder;
+import nl.tudelft.fa.core.lobby.LobbyBalancerInformation;
+import nl.tudelft.fa.core.lobby.LobbyConfiguration;
+import nl.tudelft.fa.core.lobby.LobbyInformation;
+import nl.tudelft.fa.core.lobby.LobbyStatus;
+import nl.tudelft.fa.core.lobby.message.*;
+import nl.tudelft.fa.core.user.User;
+import scala.PartialFunction;
+import scala.runtime.BoxedUnit;
+
+import java.util.*;
+
+/**
+ * A {@link LobbyBalancer} actor balances users between {@link Lobby} actors to create balanced
+ * games. It will spin up new instances if needed.
+ *
+ * @author Fabian Mastenbroek
+ */
+public class LobbyBalancer extends AbstractActor {
+    /**
+     * The configuration to use when spinning up new {@link Lobby} actors.
+     */
+    private final LobbyConfiguration configuration;
+
+    /**
+     * The minimum amount of instances that should be running at all times.
+     */
+    private final int min;
+
+    /**
+     * The maximum amount of instances that are allowed to be active.
+     */
+    private final int max;
+
+    /**
+     * The instances this balancer manages.
+     */
+    private final Map<ActorRef, LobbyInformation> instances;
+
+    /**
+     * The available instances.
+     */
+    private final Map<ActorRef, LobbyInformation> available;
+
+    /**
+     * The {@link LoggingAdapter} of this class.
+     */
+    private final LoggingAdapter log = Logging.getLogger(context().system(), this);
+
+    /**
+     * Construct a {@link LobbyBalancer} instance.
+     *
+     * @param configuration The configuration of the instances.
+     * @param min The minimum amount of instances that should be running at all times.
+     * @param max The maximum amount of instances that are allowed to be running.
+     */
+    private LobbyBalancer(LobbyConfiguration configuration, int min, int max) {
+        this.configuration = configuration;
+        this.min = min;
+        this.max = max;
+        this.instances = new HashMap<>(max);
+        this.available = new HashMap<>(max);
+    }
+
+    /**
+     * This method is invoked before the start of this actor.
+     */
+    @Override
+    public void preStart() {
+        // Spin up minimum amount of instances
+        for (int i = 0; i < min; i++) {
+            createLobby();
+        }
+    }
+
+    /**
+     * This method defines the initial actor behavior, it must return a partial function with the
+     * actor logic.
+     *
+     * @return The initial actor behavior as a partial function.
+     */
+    @Override
+    public PartialFunction<Object, BoxedUnit> receive() {
+        return ReceiveBuilder
+            .match(InformationRequest.class, this::inform)
+            .match(JoinRequest.class, this::join)
+            .match(LobbyInformation.class, this::update)
+            .build();
+    }
+
+    /**
+     * Inform an actor about the current state of this {@link Lobby}.
+     *
+     * @param req The request to handle.
+     */
+    private void inform(InformationRequest req) {
+        sender().tell(new LobbyBalancerInformation(Collections.unmodifiableMap(instances)), self());
+    }
+
+    /**
+     * Route a {@link JoinRequest} to a fitting {@link Lobby} actor.
+     *
+     * @param req The join request to handle.
+     */
+    private void join(JoinRequest req) {
+        final ActorRef ref;
+
+        if (!available.isEmpty()) {
+            ref = instances.keySet().iterator().next();
+        } else if (instances.size() < max) {
+            log.info("Spinning up new lobby to meet demand");
+            ref = createLobby();
+        } else {
+            log.warning("Balancer exhausted. Cannot fulfil request {} ", req);
+            sender().tell(new LobbyBalancerExhaustedError(), self());
+            return;
+        }
+
+        log.debug("Routing request {} from {} to {}", req, sender(), ref);
+
+        ref.tell(req, sender());
+
+        // Update our cache until we get confirmation
+        LobbyInformation info = instances.get(ref);
+        Set<User> users = new HashSet<>(info.getUsers());
+        users.add(req.getUser());
+        info = new LobbyInformation(info.getStatus(), info.getConfiguration(), users);
+        instances.put(ref, info);
+        available.put(ref, info);
+    }
+
+    /**
+     * Update the information of each {@link Lobby} this balancer manages.
+     *
+     * @param information The information that has been received.
+     */
+    private void update(LobbyInformation information) {
+        if (information.getUsers().size() == 0 && instances.size() > min) {
+            log.debug("Lobby {} is empty and will be killed", sender());
+
+            // Kill the lobby if it is empty
+            context().stop(sender());
+            instances.remove(sender());
+            available.remove(sender());
+            return;
+        } else if (information.getUsers().size()
+                == information.getConfiguration().getPlayerMaximum()) {
+            log.debug("Lobby {} is full and will be forgotten", sender());
+
+            // Forget the lobby if it is full
+            available.remove(sender());
+        } else {
+            // Update the information of the available lobby
+            available.put(sender(), information);
+        }
+        instances.put(sender(), information);
+    }
+
+    /**
+     * Spin up a new {@link Lobby} instance.
+     *
+     * @return The reference to the {@link Lobby} actor that has been created.
+     */
+    private ActorRef createLobby() {
+        ActorRef ref = context().actorOf(Lobby.props(configuration), UUID.randomUUID().toString());
+        LobbyInformation info = new LobbyInformation(LobbyStatus.PREPARATION, configuration,
+            Collections.emptySet());
+        instances.put(ref, info);
+        available.put(ref, info);
+        return ref;
+    }
+
+    /**
+     * Create {@link Props} instance for an actor of this type.
+     *
+     * @param configuration The configuration of the lobby.
+     * @param min The minimum amount of instances that should be running at all times.
+     * @param max The maximum amount of instances that are allowed to be active.
+     * @return A Props for creating this actor, which can then be further configured
+     *         (e.g. calling `.withDispatcher()` on it)
+     */
+    public static Props props(LobbyConfiguration configuration, int min, int max) {
+        return Props.create(LobbyBalancer.class, () -> new LobbyBalancer(configuration, min, max));
+    }
+
+    /**
+     * Create {@link Props} instance for an actor of this type.
+     *
+     * <p>
+     *     This balancer will create at most 10 lobbies and at least 2 lobbies are active are
+     *     active at all times.
+     * </p>
+     *
+     * @param configuration The configuration of the lobby.
+     * @return A Props for creating this actor, which can then be further configured
+     *         (e.g. calling `.withDispatcher()` on it)
+     */
+    public static Props props(LobbyConfiguration configuration) {
+        return props(configuration, 2, 10);
+    }
+}
