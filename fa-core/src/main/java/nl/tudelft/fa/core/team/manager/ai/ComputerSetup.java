6c65ca088001825419337f5d3cfae38c54cf52ec
Nikki Bouman <nikki_bouman@live.nl>
2017-01-15 12:44:56 +0100

    Change a weird error in getRaining(), continued with the AI (ComputerSetup)

--- fa-core/src/main/java/nl/tudelft/fa/core/team/manager/ai/ComputerSetup.java
+++ fa-core/src/main/java/nl/tudelft/fa/core/team/manager/ai/ComputerSetup.java
@@ -1,11 +1,13 @@
 package nl.tudelft.fa.core.team.manager.ai;
 
 import nl.tudelft.fa.core.game.RaceSimulator;
+import nl.tudelft.fa.core.race.CarConfiguration;
 import nl.tudelft.fa.core.race.CarParameters;
 import nl.tudelft.fa.core.team.Aerodynamicist;
 import nl.tudelft.fa.core.team.Driver;
 import nl.tudelft.fa.core.team.Mechanic;
 import nl.tudelft.fa.core.team.Strategist;
+import nl.tudelft.fa.core.team.inventory.Car;
 import nl.tudelft.fa.core.team.inventory.Engine;
 import nl.tudelft.fa.core.team.inventory.Tire;
 import nl.tudelft.fa.core.team.inventory.TireType;
@@ -14,17 +16,19 @@ import java.util.Random;
 import java.util.UUID;
 
 /**
- * Created by nikki on 1/9/2017.
+ * This class is for the AI of the computer.
+ * Now it will choose a random car with its engine and risk.
  */
 public class ComputerSetup {
 
-    Random rand = new Random();
+    private Random rand = new Random();
     private Tire computerTire = new Tire(UUID.randomUUID(), "", TireType.INTERMEDIATE, 1, 1);
     private Engine computerEngine = new Engine(UUID.randomUUID(), "Renault", "", 1, 1, 1);
     private Driver computerDriver = new Driver(UUID.randomUUID(), "", 100, 100, 100, 100);
     private Aerodynamicist computerAerodynamicist = new Aerodynamicist(UUID.randomUUID(), "", 100, 1);
     private Strategist computerStrategist = new Strategist(UUID.randomUUID(), "", 100, 1);
     private Mechanic computerMechanic = new Mechanic(UUID.randomUUID(), "", 100, 1);
+    private Car newcar = new Car(UUID.randomUUID());
 
     /**
      * Computer chooses a random tire, depending on if it's raining or not.
@@ -78,6 +82,7 @@ public class ComputerSetup {
         }
     }
 
+
     /**
      * Method to choose random risks and create a new CarParameters with the risks and the chosen tire (from chooseTire) in it.
      */
@@ -88,7 +93,18 @@ public class ComputerSetup {
         new CarParameters(m, a, s, computerTire);
     }
 
-    //new CarConfiguration(car, computerEngine, computerDriver, computerMechanic, computerAerodynamicist, computerStrategist);
-
+    /**
+     * Creates the actual car for the computer
+     * @param newcar is the variable for chosen Car
+     * @param computerEngine is the variable for the chosen Engine
+     * @param computerDriver is the variable for the chose Driver
+     * @param computerMechanic is the variable for the chosen Mechanic
+     * @param computerAerodynamicist is the variable for the chosen Aerodynamicist
+     * @param computerStrategist is the variable for the chosen Strategist
+     * @return returns the created Car (CarConfiguration)
+     */
+    public CarConfiguration createCar(Car newcar, Engine computerEngine, Driver computerDriver, Mechanic computerMechanic, Aerodynamicist computerAerodynamicist, Strategist computerStrategist) {
+        return new CarConfiguration(newcar, computerEngine, computerDriver, computerMechanic, computerAerodynamicist, computerStrategist);
+    }
 
 }
