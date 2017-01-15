7257e131fee64eb240e2a4601a970d45c351aac9
ChristovS <fchristianslothouber@gmail.com>
2017-01-15 22:19:36 +0100

    Adding basic store GUI.

---
+++ fa-frontend-javafx/src/main/java/nl/tudelft/fa/frontend/javafx/controller/StoreController.java
@@ -0,0 +1,18 @@
+package nl.tudelft.fa.frontend.javafx.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by fchri on 15-1-2017.
+ */
+public class StoreController {
+
+
+}
+
