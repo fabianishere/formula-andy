264f6e580ee63a9fa8e8dc603c63c7cecf7cc816
ChristovS <fchristianslothouber@gmail.com>
2017-01-17 16:37:19 +0100

    ..

--- fa-frontend-javafx/src/main/java/nl/tudelft/fa/frontend/javafx/Main.java
+++ fa-frontend-javafx/src/main/java/nl/tudelft/fa/frontend/javafx/Main.java
@@ -46,7 +46,7 @@ public class Main extends Application {
      */
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("setup-screen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("login-screen.fxml"));
 
         Scene scene = new Scene(root);
         stage.setTitle("Formula Andy!");
